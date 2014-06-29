4f73c66910c9ecbf6948b8d586cf4a12ba08235a
tkmagesh <tkmagesh77@gmail.com>
2014-06-29 11:20:32 +0530

    timer bug - fix

--- Day-3/Node/WebSocketsDemo/CiscoSocketServer/timeServer.js
+++ Day-3/Node/WebSocketsDemo/CiscoSocketServer/timeServer.js
@@ -1,19 +1,23 @@
 var webSocketModule = require("nodejs-websocket");
-var timer;
 var wsServer = webSocketModule.createServer(function(connection){
-	connection.on("text",function(msg){
-		if (msg === "time"){
-			console.log(msg);
-			timer = setInterval(function(){
-				console.log("timeSent");
-				connection.sendText(new Date().toString());
-			},5000);
-		}
-		if (msg === "stop"){
-			clearInterval(timer);
-			connection.sendText("timer stopped");
-		}
-	});
+	connection.on("text",(function(){
+		var timer;
+		return function(msg){
+			if (msg === "time"){
+				console.log(msg);
+				timer = setInterval(function(){
+					console.log("timeSent");
+					connection.sendText(new Date().toString());
+				},5000);
+				console.log(timer);
+			}
+			if (msg === "stop"){
+				clearInterval(timer);
+				connection.sendText("timer stopped");
+				console.log(timer);
+			}
+		};
+	})());
 });
 wsServer.listen(9090);
 console.log("the web socket server is listening on port 9090");
